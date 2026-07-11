f30bf1cb6c920f9a12cfa9eb1a6d0e70ebc59761
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2026-07-11 10:40:40 -0400

    Implement command-line input handling for trending repos and exit command

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+process.stdin.on("data", (data) => {
+    const input = data.toString().trim()
+    const inputSplitted = input.split(" ")
+ 
+    const command = inputSplitted[0]
+    const filters = inputSplitted.slice(1)
+
+    switch(command) {
+        case "trending-repos":
+            console.log("Trending")
+            break;
+        case "exit":
+            process.exit()
+        default: 
+            console.log("Incorrect option.")
+    }
+}) 
